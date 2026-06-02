4a0cedce139a9c99e905ac9b4837256307c20cf8
ROHAN DAS P <rohan2003710@gmail.com>
2026-06-02 21:05:58 +0530

    added the page folder and its functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,19 +9,13 @@ import Login from './pages/Login';
 import PlaceOrder from './pages/PlaceOrder';
 import Orders from './pages/Orders';
 import Collection from './pages/Collection'; 
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import SearchBar from './components/SearchBar';
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
 
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      <ToastContainer/>
-      <NavBar />
-      <SearchBar/>
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/collection" element={<Collection />} />
@@ -33,7 +27,7 @@ const App = () => {
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="/orders" element={<Orders />} />
       </Routes>
-      <Footer/>
+
     </div>
   );
 };

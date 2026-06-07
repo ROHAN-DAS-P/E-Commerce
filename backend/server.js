50e84476bdd11db78c881b587fefccc75a0de741
ROHAN DAS P <rohan2003710@gmail.com>
2026-06-07 22:58:00 +0530

    implemented the add product logic and created a multer middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 
 //App config
@@ -16,6 +17,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
 app.get('/', (req, res) => {
     res.send("API working")
 })
